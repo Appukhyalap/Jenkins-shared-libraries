760be364acd0dee69fa06aede36d00a2d5fede61
Appukhyalap <152011329+Appukhyalap@users.noreply.github.com>
2026-01-16 22:08:06 +0530

    added docker_push

---
+++ vars/docker_push.groovy
@@ -0,0 +1,7 @@
+def call(String ProjectName, String Tag, String dockerHubUser){
+    withCredentials([usernamePassword(credentialsId:"dockerHubCred", usernameVariable:"dockerHubUser", passwordVariable:"dockerHubPass")]){
+        sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
+    }
+    sh "docker push ${dockerHubUser}/notes-app:latest"
+  }
+}
