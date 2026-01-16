5bc227dd53fa2822199a2b92b95d78c7fe581ea4
Appukhyalap <152011329+Appukhyalap@users.noreply.github.com>
2026-01-16 21:53:25 +0530

    configured dockerBuild

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,3 @@
 def call(String ProjectName, String Tag, String DockerHubUser){
-  sh "docker build -t ${DockerHubUser}/${ProjectName}:${Tag}"
+  sh "docker build -t ${DockerHubUser}/${ProjectName}:${Tag} ."
 }
