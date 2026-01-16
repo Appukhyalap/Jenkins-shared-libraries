28329e9bf13d1b89ff35b4a55323b1a6c6d10de9
Appukhyalap <152011329+Appukhyalap@users.noreply.github.com>
2026-01-16 22:30:31 +0530

    edited missed braces

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,6 @@
-def call(String ProjectName, String Tag, String dockerHubUser){
+def call(String ProjectName, String Tag, String dockerhubuser){
     withCredentials([usernamePassword(credentialsId:"dockerHubCred", usernameVariable:"dockerHubUser", passwordVariable:"dockerHubPass")]){
         sh "docker login -u ${dockerHubUser} -p ${dockerHubPass}"
     }
     sh "docker push ${dockerHubUser}/${ProjectName}:${Tag}"
-  }
 }
